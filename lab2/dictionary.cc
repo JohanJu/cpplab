fb0cd26b9d73015ffba2f3c74e7b9faee847e0bd
Johan Ju <johanj93@gmail.com>
2016-02-07 19:58:33 +0100

    done

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -4,13 +4,20 @@
 #include <iostream>
 #include <sstream>
 #include <algorithm>
-
 #include "dictionary.h"
 
 using namespace std;
 
+struct Pair{
+	int cost;
+	string word;
+	bool operator<(Pair& p){
+		return cost<p.cost;
+	}
+};
+
 Dictionary::Dictionary() {
-	ifstream in("small");
+	ifstream in("triWord");
 	string line, first, t;
 	unsigned nbr;
 	while(getline(in,line)){
@@ -33,26 +40,7 @@ bool Dictionary::contains(const string& word) const {
 	return raw.count(word);
 }
 
-// void Dictionary::add_trigram_suggestions(vector<string>& suggestions,const string& word){
-// 	unsigned len = word.length();
-// 	if (len > 2) {
-// 		vector<string> tri;
-// 		for (unsigned nbr = 0; nbr < len - 2; ++nbr)
-// 		{
-// 			tri.push_back(word.substr(nbr, 3));
-// 		}
-// 		sort(tri.begin(), tri.end());
-// 		for (Word w : words[len])
-// 		{
-// 			if(w.get_matches(tri)){
-// 				suggestions.push_back(w.get_word());
-// 			}
-// 		}
-// 	}
-// }
-
-vector<string> Dictionary::get_suggestions(const string& word) const {
-	vector<string> suggestions;
+void Dictionary::add_trigram_suggestions(vector<string>& suggestions,const string& word) const{
 	unsigned len = word.length();
 	if (len > 2) {
 		vector<string> tri;
@@ -61,21 +49,17 @@ vector<string> Dictionary::get_suggestions(const string& word) const {
 			tri.push_back(word.substr(nbr, 3));
 		}
 		sort(tri.begin(), tri.end());
-		// for (string s : tri)
-		// {
-		// 	cout << s << " ";
-		// }
 		for (Word w : words[len])
 		{
 			unsigned u = w.get_matches(tri);
-			if (u > 0 && u > len - 5) {
+			if (u > 0) {
 				suggestions.push_back(w.get_word());
 			}
 		}
 		for (Word w : words[len + 1])
 		{
 			unsigned u = w.get_matches(tri);
-			if (u > 0 && u > len - 4) {
+			if (u > 0) {
 
 				suggestions.push_back(w.get_word());
 			}
@@ -84,23 +68,70 @@ vector<string> Dictionary::get_suggestions(const string& word) const {
 			for (Word w : words[len - 1])
 			{
 				unsigned u = w.get_matches(tri);
-				if (u > 0 && u > len - 6) {
+				if (u > 0) {
 					suggestions.push_back(w.get_word());
 				}
 			}
 		}
 	}
-	// for (string s : suggestions)
-	// {
-	// 	cout << s << endl;
-	// }
+}
+
+void Dictionary::rank_suggestions(vector<string>& suggestions,const string& word) const {
+	vector<Pair> ranked;
+	int d[26][26];
+	for (int i = 0; i < 26; ++i)
+	{
+		d[i][0] = i;
+		d[0][i] = i;
+	}
+	for(string s : suggestions){
+		for (unsigned i = 1; i <= word.length(); ++i)
+		{
+			for (unsigned j = 1; j <= s.length(); ++j)
+			{
+				int a,b,c;
+				a = word[i]==s[j]?d[i-1][j-1]:d[i-1][j-1]+1;
+				b = d[i-1][j]+1;
+				c = d[i][j - 1] + 1;
+				d[i][j] = a < b ? (a < c ? a : c) : (b < c ? b : c);
+			}
+		}
+		int cost = d[word.length()][s.length()];
+		ranked.push_back(Pair{cost, s});
+	}
+	sort(ranked.begin(), ranked.end());
+	suggestions.resize(0);
+	int max = 5>ranked.size()?ranked.size():5;
+	for (int i = 0; i < max; ++i)
+	{
+		suggestions.push_back(ranked[i].word);
+	}
+}
+
+vector<string> Dictionary::get_suggestions(const string& word) const {
+	vector<string> suggestions;
+	Dictionary::add_trigram_suggestions(suggestions,word);
+	Dictionary::rank_suggestions(suggestions,word);
 	return suggestions;
 }
 
-int main()
-{
-	// cout << "dic" <<endl;
-	Dictionary d;
-	vector<string> v = d.get_suggestions("ariel");
-	return 0;
-}
+// int main()
+// {
+// 	// int a = 7,b =6 ,c=4,d;
+// 	// d = a<b?(a<c?a:c):(b<c?b:c);
+// 	// cout << d <<endl;
+
+// 	// Pair a{1,"a"}, b{5,"b"}, c{2,"c"};
+// 	// vector<Pair> v;
+// 	// v.push_back(a);
+// 	// v.push_back(b);
+// 	// v.push_back(c);
+// 	// sort(v.begin(),v.end());
+// 	// for(Pair& p:v){
+// 	// 	cout << p.word<< endl;
+// 	// }
+
+// 	Dictionary d;
+// 	vector<string> v = d.get_suggestions("ariel");
+// 	return 0;
+// }
--- lab2/dictionary.h
+++ lab2/dictionary.h
@@ -12,7 +12,8 @@ public:
 	bool contains(const std::string& word) const;
 	std::vector<std::string> get_suggestions(const std::string& word) const;
 private:
-	// void add_trigram_suggestions(std::vector<std::string>& suggestions,const std::string& word);
+	void add_trigram_suggestions(std::vector<std::string>& suggestions,const std::string& word) const;
+	void rank_suggestions(std::vector<std::string>& suggestions,const std::string& word) const;
 	std::unordered_set<std::string> raw;
 	std::vector<Word> words[25];
 };
