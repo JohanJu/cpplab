89cb95771621d8feaea0528491be4e4ec9341c2f
Johan Ju <johanj93@gmail.com>
2016-02-23 00:16:30 +0100

    why does it work??

--- lab4/bitset/bitsetiterator.h
+++ lab4/bitset/bitsetiterator.h
@@ -2,7 +2,7 @@
  * A BitsetIterator is an iterator for Bitsets.
  * Postfix ++, ==, and -> are not implemented.
  */
-
+#include <iostream>
 #ifndef BITSET_ITERATOR_H
 #define BITSET_ITERATOR_H
 
@@ -14,18 +14,23 @@ public:
 	BitsetIterator(Bitset::BitStorage* pb, std::size_t p) : ref(pb, p) {}
 	
 	bool operator!=(const BitsetIterator& bsi) const {
-		return true;
+		// std::cout <<  "!=" << std::endl;
+		return bsi.ref.pos!=ref.pos;
 	}
 	
 	BitsetIterator& operator++() {
+		// std::cout <<  "++" << std::endl;
+		++(ref.pos);
 		return *this;
 	}
 	
 	BitReference operator*() {
+		// std::cout <<  "*" << std::endl;
 		return ref;
 	}
 	
 	BitsetIterator& operator=(const BitsetIterator& rhs) {
+		// std::cout <<  "=" << std::endl;
 		ref.p_bits = rhs.ref.p_bits;
 		ref.pos = rhs.ref.pos;
 		return *this;
